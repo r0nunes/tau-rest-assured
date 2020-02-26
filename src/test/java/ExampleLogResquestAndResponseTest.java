49f3f71d899799ca6f86cbd19f9154ce8301b02b
Renato de Oliveira Nunes <renato.nunes@neogrid.com>
2020-02-26 14:25:29 -0300

    Ajuste POM

--- src/test/java/ExampleLogResquestAndResponseTest.java
+++ src/test/java/ExampleLogResquestAndResponseTest.java
@@ -1,8 +1,6 @@
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class ExampleLogResquestAndResponseTest {
 
